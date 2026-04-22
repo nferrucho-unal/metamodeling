6d8fa6ec5a61604562d9e47c456947b7a61350ed
nferrucho-unal <nferrucho@unal.edu.co>
2026-04-21 19:35:26 -0500

    nivelacion2

--- test/test_example.py
+++ test/test_example.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 def test_placeholder():
     """Placeholder test to ensure tests directory is recognized."""
     assert True
-=======
-
->>>>>>> f13d7a677fd566bbef24c991b38e162b029dcdc6
